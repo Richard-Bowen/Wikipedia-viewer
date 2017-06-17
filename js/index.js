5d9645f1eebb4f0e54db90843562e7e258627bc9
Richard Bowen <richard.bowen@richarddavidbowen.co.uk>
2017-06-17 08:13:59 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
+  window.location.reload(true);
   $("#search").on("click", function() {
-    var sTerm = $("#input").val(); //assign contents of the iputbox to the sTerm variable.
+    //assign contents of the inputbox to the sTerm variable.
+    var sTerm = $("#input").val();
+    //create wikipedia search string
     var Surl = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch="+sTerm+"&srnamespace=0&srwhat=text&titles=Main%20Page&callback=?" 
-    $.getJSON(Surl, function(data) { //get JSON data
+    //get JSON data and create a search result for each entry
+    $.getJSON(Surl, function(data) { 
          $.each(data.query.search,function(i,item){
            var wiki = '<div class="entry">'+
     '<div class="title">'+item.title+'</div>'+
